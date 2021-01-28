07e9012e6a271b79c976f4a21b4b647cf2309b11
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-01-28 14:36:07 -0700

    Lesson4 added.

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,3 +1,14 @@
+const year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+// long, medium, short
+const datefield = document.querySelector("date");
+datefield.textContent = fulldate;
+
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
