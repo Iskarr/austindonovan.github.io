bb2477adffd130f1d47bbebefbb280496ed4a263
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-03-01 15:26:10 -0700

    added prophet page and included to portal

---
+++ lesson9/js/aside.js
@@ -0,0 +1,5 @@
+if (currentDay == 5) {
+  document.getElementById("aside").style.display = "block";
+} else {
+  document.getElementById("aside").style.display = "none";
+}
--- lesson9/js/scripts.js
+++ lesson9/js/scripts.js
@@ -1,11 +1,5 @@
 let currentDay = new Date().getDay();
 
-if (currentDay == 5) {
-  document.getElementById("aside").style.display = "block";
-} else {
-  document.getElementById("aside").style.display = "none";
-}
-
 const year = new Date().getFullYear();
 document.getElementById("year").innerHTML = year;
 
