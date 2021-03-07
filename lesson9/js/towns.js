e6a6fc23bee8a93f9c00dc48c9b544aebbb146a9
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-03-06 22:24:04 -0700

    updated index page

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -7,34 +7,38 @@ fetch(requestURL)
   .then(function (jsonObject) {
     //console.table(jsonObject); // temporary checking for valid response and data parsing
     const towns = jsonObject["towns"];
+    const mainTowns = [towns[1], towns[5], towns[6]];
 
-    for (let i = 0; i < towns.length; i++) {
+    for (let i = 0; i < mainTowns.length; i++) {
       // creates card and places prophet name in h2 element
       let card = document.createElement("article");
       let h2 = document.createElement("h2");
-      h2.textContent = towns[i].name;
+      h2.textContent = mainTowns[i].name;
       card.appendChild(h2);
       document.querySelector("div.grid").appendChild(card);
 
+      card.setAttribute("class", "mainTowns");
+
       // year founded
       let founding = document.createElement("p");
-      founding.textContent = "Year Founded: " + towns[i].yearFounded;
+      founding.textContent = "Year Founded: " + mainTowns[i].yearFounded;
       card.appendChild(founding);
 
       // population
       let population = document.createElement("p");
-      population.textContent = "Population: " + towns[i].currentPopulation;
+      population.textContent = "Population: " + mainTowns[i].currentPopulation;
       card.appendChild(population);
       // rainfall average
       let rainfallAvg = document.createElement("p");
-      rainfallAvg.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+      rainfallAvg.textContent =
+        "Average Rainfall: " + mainTowns[i].averageRainfall;
       card.appendChild(rainfallAvg);
 
       // set the town image
       let image = document.createElement("img");
       image.setAttribute("loading", "lazy");
-      image.setAttribute("src", towns[i].photo);
-      image.setAttribute("alt", towns[i].name);
+      image.setAttribute("src", mainTowns[i].photo);
+      image.setAttribute("alt", mainTowns[i].name);
       card.appendChild(image);
     }
   });
