25425f8808ba1cf79d7616183f16e95990ce1d46
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-03-16 10:51:48 -0600

    updated all pages to drop summary down.

--- lesson11/js/fishhaven/town-data.js
+++ lesson11/js/fishhaven/town-data.js
@@ -5,28 +5,28 @@ fetch(townURL)
   .then((jsObject) => {
     //console.log(jsObject);
     const preston = jsObject;
-    console.log(preston);
+    // console.log(preston);
 
     document.getElementById("comingEvent").innerHTML =
       preston.towns[6].events[0];
 
     for (let i = 0; i < preston.towns[6].events.length; i++) {
-      console.log(preston.towns[6].events.length);
+      //console.log(preston.towns[6].events.length);
       // creates the elements to render
       let card = document.createElement("article");
       let h2 = document.createElement("h2");
-      let p = document.createElement("p");
+      let h3 = document.createElement("h3");
       let image = document.createElement("img");
 
-      h2.textContent = preston.towns[6].name + " Events";
-      p.textContent = preston.towns[6].events[i];
+      h2.textContent = preston.towns[6].name + " Event:";
+      h3.textContent = preston.towns[6].events[i];
       image.setAttribute("loading", "lazy");
       image.setAttribute("src", preston.towns[6].photo);
       image.setAttribute("alt", preston.towns[6].name);
 
       // allows the dates to show
       card.appendChild(h2);
-      card.appendChild(p);
+      card.appendChild(h3);
       card.appendChild(image);
       document.querySelector("div.grid").appendChild(card);
     }
