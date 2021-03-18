c96c07f37eac6a3fd7c1cdabf03f1b9ada51ebf2
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-03-18 12:52:58 -0600

    updating performance

--- lesson11/js/imageApi.js
+++ lesson11/js/imageApi.js
@@ -1,18 +1,17 @@
-// imgsURL = "https://picsum.photos/v2/list";
 imgsURL = "https://picsum.photos/v2/list";
 
 fetch(imgsURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    // console.log(jsObject);
     const imageList = jsObject;
     let num = 0;
     for (let i = 0; i < 9; i++) {
       let imageURLs = imageList[num + 1].download_url;
-      console.log(imageURLs);
+      // console.log(imageURLs);
 
       document.getElementById(`image${num + 1}`).setAttribute("src", imageURLs);
       num++;
-      console.log(num);
+      // console.log(num);
     }
   });
