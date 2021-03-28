195c1633c49946e536496d0fa6d475ea7c586340
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-03-28 00:03:05 -0600

    minor updates

--- Chamber/js/testapi.js
+++ Chamber/js/testapi.js
@@ -1,1 +1,12 @@
-const fetchAPI = "";
+const cityAPI =
+  "https://iskarr.github.io/austindonovan.github.io/api/city.json";
+
+fetch(cityAPI)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const cityList = jsObject;
+    // this will get each city from 0-X depending on city amount.
+    console.log(cityList.prophets[1]);
+  });
