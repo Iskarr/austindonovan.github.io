fac26bd49265541cb55fece4c43d786e82639d40
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-03-31 21:44:53 -0600

    updating api

--- Chamber/js/imageApi.js
+++ Chamber/js/imageApi.js
@@ -1,17 +1,21 @@
+businessAPI =
+  "https://iskarr.github.io/austindonovan.github.io/api/business.json";
+
 imgsURL = "https://picsum.photos/v2/list";
 
-fetch(imgsURL)
+fetch(businessAPI)
   .then((response) => response.json())
   .then((jsObject) => {
     // console.log(jsObject);
     const imageList = jsObject;
+    console.log(imageList.business[1].imageurl);
     let num = 0;
     for (let i = 0; i < 9; i++) {
-      let imageURLs = imageList[num + 1].download_url;
+      let imageURLs = imageList[num + 1].imageurl;
       // console.log(imageURLs);
 
       document.getElementById(`image${num + 1}`).setAttribute("src", imageURLs);
       num++;
-      // console.log(num);
+      console.log(num);
     }
   });
