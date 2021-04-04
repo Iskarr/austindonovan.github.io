69c538575f42fabd887e417659e8fcfc4affce54
Iskarr <32428946+Iskarr@users.noreply.github.com>
2021-04-03 19:04:21 -0600

    updating businesses

---
+++ Chamber/js/tooele.js
@@ -0,0 +1,18 @@
+const cityAPI =
+  "https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest";
+
+function addCommas(n) {
+  return format(n);
+}
+fetch(cityAPI)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    // console.log(jsObject);
+
+    const cityList = jsObject.data[44];
+
+    document.getElementById("IDyear").textContent = cityList.Year;
+    document.getElementById("population").textContent = cityList.Population;
+
+    // this will get each city from 0-X depending on city amount.
+  });
